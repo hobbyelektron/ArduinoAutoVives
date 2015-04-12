9f3ea71d21503b1c88593d8bce375919293e424b
Sam Decoster <decostersam@gmail.com>
2015-04-12 17:58:50 +0200

    Add PC software C#

---
+++ PC software/auto controller/auto controller/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace auto_controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnVooruit = new System.Windows.Forms.Button();
+            this.btnAchteruit = new System.Windows.Forms.Button();
+            this.btnLinks = new System.Windows.Forms.Button();
+            this.btnRechts = new System.Windows.Forms.Button();
+            this.btnLichtAAN = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.txbComPoort = new System.Windows.Forms.TextBox();
+            this.lblComPoort = new System.Windows.Forms.Label();
+            this.btnLicht2Aan = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblServo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::auto_controller.Properties.Resources.logo;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Name = "label1";
+            // 
+            // btnVooruit
+            // 
+            resources.ApplyResources(this.btnVooruit, "btnVooruit");
+            this.btnVooruit.Name = "btnVooruit";
+            this.btnVooruit.UseVisualStyleBackColor = true;
+            this.btnVooruit.Click += new System.EventHandler(this.btnVooruit_Click);
+            // 
+            // btnAchteruit
+            // 
+            resources.ApplyResources(this.btnAchteruit, "btnAchteruit");
+            this.btnAchteruit.Name = "btnAchteruit";
+            this.btnAchteruit.UseVisualStyleBackColor = true;
+            this.btnAchteruit.Click += new System.EventHandler(this.btnAchteruit_Click);
+            // 
+            // btnLinks
+            // 
+            resources.ApplyResources(this.btnLinks, "btnLinks");
+            this.btnLinks.Name = "btnLinks";
+            this.btnLinks.UseVisualStyleBackColor = true;
+            // 
+            // btnRechts
+            // 
+            resources.ApplyResources(this.btnRechts, "btnRechts");
+            this.btnRechts.Name = "btnRechts";
+            this.btnRechts.UseVisualStyleBackColor = true;
+            // 
+            // btnLichtAAN
+            // 
+            resources.ApplyResources(this.btnLichtAAN, "btnLichtAAN");
+            this.btnLichtAAN.Name = "btnLichtAAN";
+            this.btnLichtAAN.UseVisualStyleBackColor = true;
+            this.btnLichtAAN.Click += new System.EventHandler(this.btnLichtAAN_Click);
+            // 
+            // btnConnect
+            // 
+            resources.ApplyResources(this.btnConnect, "btnConnect");
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnStop
+            // 
+            resources.ApplyResources(this.btnStop, "btnStop");
+            this.btnStop.Name = "btnStop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // trackBar1
+            // 
+            resources.ApplyResources(this.trackBar1, "trackBar1");
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // txbComPoort
+            // 
+            resources.ApplyResources(this.txbComPoort, "txbComPoort");
+            this.txbComPoort.Name = "txbComPoort";
+            // 
+            // lblComPoort
+            // 
+            resources.ApplyResources(this.lblComPoort, "lblComPoort");
+            this.lblComPoort.Name = "lblComPoort";
+            // 
+            // btnLicht2Aan
+            // 
+            resources.ApplyResources(this.btnLicht2Aan, "btnLicht2Aan");
+            this.btnLicht2Aan.Name = "btnLicht2Aan";
+            this.btnLicht2Aan.UseVisualStyleBackColor = true;
+            this.btnLicht2Aan.Click += new System.EventHandler(this.btnLicht2Aan_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // lblStatus
+            // 
+            resources.ApplyResources(this.lblStatus, "lblStatus");
+            this.lblStatus.Name = "lblStatus";
+            // 
+            // lblServo
+            // 
+            resources.ApplyResources(this.lblServo, "lblServo");
+            this.lblServo.Name = "lblServo";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblServo);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLicht2Aan);
+            this.Controls.Add(this.lblComPoort);
+            this.Controls.Add(this.txbComPoort);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnLichtAAN);
+            this.Controls.Add(this.btnRechts);
+            this.Controls.Add(this.btnLinks);
+            this.Controls.Add(this.btnAchteruit);
+            this.Controls.Add(this.btnVooruit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVooruit;
+        private System.Windows.Forms.Button btnAchteruit;
+        private System.Windows.Forms.Button btnLinks;
+        private System.Windows.Forms.Button btnRechts;
+        private System.Windows.Forms.Button btnLichtAAN;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox txbComPoort;
+        private System.Windows.Forms.Label lblComPoort;
+        private System.Windows.Forms.Button btnLicht2Aan;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblServo;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PC software/auto controller/auto controller/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace auto_controller
+{
+    public partial class Form1 : Form
+    {
+        byte Verlichting = Convert.ToByte(0);
+        byte vooruit = 0;
+        byte achteruit = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Verlichting = Convert.ToByte(0);
+        }
+
+        private void btnVooruit_Click(object sender, EventArgs e)
+        {
+            vooruit = Convert.ToByte(255);
+            achteruit = Convert.ToByte(0);
+        }
+
+        //BT verbindings gedeelte
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (btnConnect.Text == "Verbind")
+                {
+                    lblStatus.Text = "Bezig met verbinden!";
+                    serialPort1.PortName = txbComPoort.Text;
+                    serialPort1.Open();
+                    timer1.Enabled = true;
+                    lblStatus.Text = "Verbonden!";
+                    btnConnect.Text = "Verbreek";
+                }
+                else if (btnConnect.Text == "Verbreek")
+                {
+                    lblStatus.Text = "Bezig met verbreken";
+                    timer1.Enabled = false;
+                    serialPort1.Close();
+                    lblStatus.Text = "Niet verbonden!";
+                    btnConnect.Text = "Verbind";
+                }
+            }
+            catch
+            {
+                lblStatus.Text = "Fout bij het verbinden!";
+                MessageBox.Show("Fout bij het verbinden!");
+            }
+            
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            vooruit = Convert.ToByte(0);
+            achteruit = Convert.ToByte(0);
+        }
+
+        private void btnAchteruit_Click(object sender, EventArgs e)
+        {
+            achteruit = Convert.ToByte(255);
+            vooruit = Convert.ToByte(0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] data = new byte[6];
+
+                data[5] = Convert.ToByte(trackBar1.Value);
+                data[4] = Verlichting;
+                data[3] = achteruit;
+                data[2] = vooruit;
+                data[1] = Convert.ToByte(0);
+                data[0] = Convert.ToByte(0);
+                serialPort1.Write(data, 0, data.Length);
+            }
+            catch
+            {
+                timer1.Enabled = false;
+                serialPort1.Close();
+                lblStatus.Text = "Verbinding verbroken!";
+                MessageBox.Show("Verbinding verbroken!");
+            }
+            
+        }
+        private void btnLicht2Aan_Click(object sender, EventArgs e)
+        {
+            if (Verlichting == Convert.ToByte(0))
+            {
+                Verlichting = Convert.ToByte(1);
+                btnLicht2Aan.Text = "Voorlicht UIT";
+            }
+            else if (Verlichting == Convert.ToByte(1))
+            {
+                Verlichting = Convert.ToByte(0);
+                btnLicht2Aan.Text = "Voorlicht AAN";
+            }
+            else if (Verlichting == Convert.ToByte(2))
+            {
+                Verlichting = Convert.ToByte(3);
+                btnLicht2Aan.Text = "Voorlicht UIT";
+            }
+            else if (Verlichting == Convert.ToByte(3))
+            {
+                Verlichting = Convert.ToByte(2);
+                btnLicht2Aan.Text = "Voorlicht AAN";
+            }
+        }
+
+        
+        private void btnLichtAAN_Click(object sender, EventArgs e)
+        {
+            if (Verlichting == Convert.ToByte(0))
+            {
+                Verlichting = Convert.ToByte(2);
+                btnLichtAAN.Text = "Achterlicht UIT";
+            }
+            else if (Verlichting == Convert.ToByte(1))
+            {
+                Verlichting = Convert.ToByte(3);
+                btnLichtAAN.Text = "Achterlicht UIT";
+            }
+            else if (Verlichting == Convert.ToByte(2))
+            {
+                Verlichting = Convert.ToByte(0);
+                btnLichtAAN.Text = "Achterlicht AAN";
+            }
+            else if (Verlichting == Convert.ToByte(3))
+            {
+                Verlichting = Convert.ToByte(1);
+                btnLichtAAN.Text = "Achterlicht AAN";
+            }
+        }
+        //Bij verandering van scrollbaar
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            lblServo.Text = "Servo: " + trackBar1.Value + "°";
+        } 
+    }
+}
